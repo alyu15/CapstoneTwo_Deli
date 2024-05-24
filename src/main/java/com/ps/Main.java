f06c145b9b235965437c064c9a49eeef61e95e38
Alisha Yu <alisha.yu15@gmail.com>
2024-05-24 10:23:22 -0400

    Added pseudocode

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -2,6 +2,33 @@ package com.ps;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Home Screen
+            // (1) New Order - creates a new order
+            // (0) Exit - exits application
+        // Order Screen
+            // (1) Add Sandwich
+                // Select your bread
+                // Select sandwich size
+                // Toppings - the user should be able to add extras of each topping
+                    // Meat
+                    // Cheese
+                    // Other toppings
+                    // Select sauces
+                    // Toasted?
+            // (2) Add Drink
+                // Select Drink size and flavor
+            // (3) Add Chips
+                // Select chip type
+            // (4) Checkout - displays the order details and the price
+                // Confirm
+                    // Creates the receipt file and goes back to home screen
+                // Cancel
+                    // Deletes the order and goes back to the home screen
+            // (0) Cancel Order
+                // Deletes the order and go back to the home screen
+
+
+
     }
 }
