d973168b16e04e3b44034254f3651b4ed1d0cf95
Alisha <alisha.yu15@gmail.com>
2024-05-24 15:33:43 -0400

    finished layout for adding drinks+chips

--- src/main/java/com/ps/OrderScreen.java
+++ src/main/java/com/ps/OrderScreen.java
@@ -1,21 +1,104 @@
 package com.ps;
 
+import java.util.Scanner;
+
 public class OrderScreen {
+static Scanner scanner = new Scanner(System.in);
     public static void handleAddDrink() {
         // (2) Add Drink
             // Select Drink size and flavor
                 // 3 sizes
                     // Small, Medium, Large
         System.out.println("Please select the size of the drink you would like:");
-        System.out.println("(1) Small");
-        System.out.println("(2) Medium");
-        System.out.println("(3) Large");
+        System.out.println("(1) Small \n(2) Medium \n(3) Large");
+        String drinkSizeInput;
+        boolean running = true;
+        while(running) {
+            drinkSizeInput = scanner.next().trim();
+
+            switch (drinkSizeInput) {
+                case "1":
+                    break;
+
+                case "2":
+                    break;
+
+                case "3":
+                    break;
+
+                default:
+                    System.out.println("Error, please select a size");
+                    continue;
+            }
+            running = false;
+        }
+        System.out.println("Please select one of the following flavors for your drink:");
+        System.out.println("(1) Sproot\n(2) Fonta\n(3) Cream Soda\n(4) Snapple\n(5) Sweet Tea");
+        String drinkFlavorInput;
+        running = true;
+
+        while(running) {
+            drinkFlavorInput = scanner.next().trim();
+
+            switch(drinkFlavorInput) {
+                case "1":
+                    break;
+
+                case "2":
+                    break;
+
+                case "3":
+                    break;
+
+                case "4":
+                    break;
+
+                case "5":
+                    break;
+
+                default:
+                    System.out.println("Error, please select a listed flavor");
+                    continue;
+
+            }
+            running = false;
+        }
     }
 
     public static void handleAddChips() {
         // (3) Add Chips
             // Select chip type
+        System.out.println("Please select the chips that you want:");
+        System.out.println("(1) Cheetos\n(2) Funyons\n(3) Miss Vickie's\n(4) Deep River\n(5) North Fork");
+        String chipsInput;
+        boolean running = true;
+
+        while (running) {
+            chipsInput = scanner.next().trim();
+            switch(chipsInput) {
+                case "1":
+                    break;
+
+                case "2":
+                    break;
+
+                case "3":
+                    break;
+
+                case "4":
+                    break;
+
+                case "5":
+                    break;
+
+                default:
+                    System.out.println("Error, please select one of the listed options");
+                    continue;
+            }
+            running = false;
+        }
     }
+
 
     public static void handleCheckout() {
         // (4) Checkout - displays the order details and the price
